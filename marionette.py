Fix quaternion to euler conversion via DM Control

marionette.py
@@ -19,7 +19,6 @@ import numpy as np
 import torch
 from gym import Env
 from polymetis import RobotInterface
-from scipy.spatial.transform import Rotation as R
 
 
 # Silence OpenAI Gym warnings
@@ -97,7 +96,7 @@ class Rate:
 
 # Rotation & Quaternion Helpers
 #   =>> Reference: DM Control -- https://github.com/deepmind/dm_control/blob/main/dm_control/utils/transformations.py
-def quat2rmat(quat) -> np.ndarray:
+def quat2rmat(quat: np.ndarray) -> np.ndarray:
     """
     Return homogeneous rotation matrix from quaternion.
 
@@ -123,6 +122,127 @@ def quat2rmat(quat) -> np.ndarray:
         ),
         dtype=np.float64,
     )
+
+
+def axis_rotation(theta):
+    """Returns the theta dim, cos and sin, and blank matrix for axis rotation."""
+    n = 1 if np.isscalar(theta) else len(theta)
+    ct, st = np.cos(theta), np.sin(theta)
+    rmat = np.zeros((n, 3, 3))
+    return n, ct, st, rmat
+
+
+def rotation_x_axis(theta):
+    """
+    Returns a rotation matrix of a rotation about the X-axis.
+
+    Supports vector-valued theta, in which case the returned array is of shape (len(t), 3, 3), or (len(t), 4, 4) if
+    full=True. If theta is scalar the batch dimension is squeezed out.
+
+    Args:
+      theta: The rotation amount.
+    """
+    n, ct, st, rmat = axis_rotation(theta)
+
+    rmat[:, 0, 0:3] = np.array([[1, 0, 0]])
+    rmat[:, 1, 0:3] = np.vstack([np.zeros(n), ct, -st]).T
+    rmat[:, 2, 0:3] = np.vstack([np.zeros(n), st, ct]).T
+
+    return rmat.squeeze()
+
+
+def rotation_y_axis(theta):
+    """
+    Returns a rotation matrix of a rotation about the Y-axis.
+
+    Supports vector-valued theta, in which case the returned array is of shape (len(t), 3, 3), or (len(t), 4, 4) if
+    full=True. If theta is scalar the batch dimension is squeezed out.
+
+    Args:
+      theta: The rotation amount.
+    """
+    n, ct, st, rmat = axis_rotation(theta)
+
+    rmat[:, 0, 0:3] = np.vstack([ct, np.zeros(n), st]).T
+    rmat[:, 1, 0:3] = np.array([[0, 1, 0]])
+    rmat[:, 2, 0:3] = np.vstack([-st, np.zeros(n), ct]).T
+
+    return rmat.squeeze()
+
+
+def rotation_z_axis(theta):
+    """
+    Returns a rotation matrix of a rotation about the z-axis.
+
+    Supports vector-valued theta, in which case the returned array is of shape (len(t), 3, 3), or (len(t), 4, 4) if
+    full=True. If theta is scalar the batch dimension is squeezed out.
+
+    Args:
+      theta: The rotation amount.
+    """
+    n, ct, st, rmat = axis_rotation(theta)
+
+    rmat[:, 0, 0:3] = np.vstack([ct, -st, np.zeros(n)]).T
+    rmat[:, 1, 0:3] = np.vstack([st, ct, np.zeros(n)]).T
+    rmat[:, 2, 0:3] = np.array([[0, 0, 1]])
+
+    return rmat.squeeze()
+
+
+def euler2rmat_xyz(euler_vec: np.ndarray) -> np.ndarray:
+    """
+    Returns the rotation matrix (or transform) for the given Euler rotations.
+
+    This method composes the Rotation matrix corresponding to the given rotations r1, r2, r3 following the ordering
+    "XYZ". To be more precised, the "XYZ" ordering specifies the order of rotation matrices in the matrix
+    multiplication, e.g., performing rotX(r1) @ rotY(r2) @ rotZ(r3).
+
+    :param euler_vec: The euler angle rotations.
+
+    :return: The rotation matrix or homogenous transform corresponding to the given Euler rotation (as a matrix)
+    """
+    rotations = [{"X": rotation_x_axis, "Y": rotation_y_axis, "Z": rotation_z_axis}[c] for c in "XYZ"]
+    euler_vec = np.atleast_2d(euler_vec)
+
+    # Get matrices...
+    rots = [rotations[i](euler_vec[:, i]) for i in range(len(rotations))]
+    return rots[0].dot(rots[1]).dot(rots[2])
+
+
+def mat2quat(rmat: np.ndarray) -> np.ndarray:
+    """
+    Return quaternion from homogeneous or rotation matrix.
+
+    Args:
+        rmat: A homogeneous transform or rotation matrix
+    Returns:
+        A quaternion [w, i, j, k].
+    """
+    if rmat.shape == (3, 3):
+        tmp = np.eye(4)
+        tmp[0:3, 0:3] = rmat
+        rmat = tmp
+
+    q = np.empty((4,), dtype=np.float64)
+    t = np.trace(rmat)
+    if t > rmat[3, 3]:
+        q[0] = t
+        q[3] = rmat[1, 0] - rmat[0, 1]
+        q[2] = rmat[0, 2] - rmat[2, 0]
+        q[1] = rmat[2, 1] - rmat[1, 2]
+    else:
+        i, j, k = 0, 1, 2
+        if rmat[1, 1] > rmat[0, 0]:
+            i, j, k = 1, 2, 0
+        if rmat[2, 2] > rmat[i, i]:
+            i, j, k = 2, 0, 1
+        t = rmat[i, i] - (rmat[j, j] + rmat[k, k]) + rmat[3, 3]
+        q[i + 1] = t
+        q[j + 1] = rmat[i, j] + rmat[j, i]
+        q[k + 1] = rmat[k, i] + rmat[i, k]
+        q[0] = rmat[k, j] - rmat[j, k]
+    q *= 0.5 / np.sqrt(t * rmat[3, 3])
+    return q
 
 
 def rmat2euler_xyz(rmat: np.ndarray) -> np.ndarray:
@@ -170,6 +290,20 @@ def quat2euler(quat: np.ndarray, ordering="XYZ"):
         return rmat2euler_xyz(mat[0:3, 0:3])
     else:
         raise NotImplementedError("Quat2Euler for Ordering != XYZ is not yet defined!")
+
+
+def euler2quat(euler_vec: np.ndarray):
+    """
+    Returns the Quaternion corresponding to the provided euler angles via the strict rotation ordering "XYZ".
+
+    Args:
+        euler_vec: The euler angle rotations.
+
+    Returns:
+        quat: A quaternion [w, i, j, k]
+    """
+    rmat = euler2rmat_xyz(euler_vec)
+    return mat2quat(rmat)
 
 
 # Polymetis Environment Wrapper...
@@ -322,7 +456,7 @@ def follow() -> None:
         z = (np.cos(t) * np.cos(t)).reshape(-1, 1)
         return np.concatenate([x, y, z], axis=1)
 
-    def figure_eight(t: Union[float, np.ndarray], scale: float = 0.5) -> np.ndarray:
+    def figure_eight(t: Union[float, np.ndarray], scale: float = 0.8) -> np.ndarray:
         # Shift curve orientation to start at current gripper orientation
         curve, origin = generate_figure_eight(t) * scale, generate_figure_eight(0.0) * scale
         return curve - origin + ee_orientation
@@ -336,7 +470,7 @@ def follow() -> None:
     while curr_t < max_t:
         # Move Robot --> transform Euler angle back to quaternion...
         new_angle = figure_eight(curr_t).flatten()
-        new_quat = R.from_euler("XYZ", new_angle).as_quat()
+        new_quat = euler2quat(new_angle)
         env.step(np.concatenate([fixed_position, new_quat], axis=0))
 
         # Grab updated orientation